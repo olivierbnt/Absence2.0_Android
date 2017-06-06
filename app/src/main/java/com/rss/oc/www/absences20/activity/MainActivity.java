c7c3e725c01b1f8b5e45a2d5f9215a148df1881d
CesarNg <bricecesar.ngoy-wessessi@epfedu.fr>
2017-06-06 17:33:02 +0200

    Liens des pages du comptes etudiant

--- app/src/main/java/com/rss/oc/www/absences20/activity/MainActivity.java
+++ app/src/main/java/com/rss/oc/www/absences20/activity/MainActivity.java
@@ -1,13 +1,18 @@
 package com.rss.oc.www.absences20.activity;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.MenuPopupWindow;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import com.rss.oc.www.absences20.R;
 import com.yalantis.guillotine.animation.GuillotineAnimation;
@@ -25,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
     FrameLayout root;
     @BindView(R.id.content_hamburger)
     View contentHamburger;
+    View ItemAccueil;
+    View ItemAbsence;
+    View ItemProfile;
+    View ItemParametres;
+    View ItemDeconnection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +43,123 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
+        View guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine, null);
+        root.addView(guillotineMenu);
+
+        ItemAccueil = guillotineMenu.findViewById(R.id.accueil_group);
+        ItemAbsence = guillotineMenu.findViewById(R.id.absence_group);
+        ItemProfile = guillotineMenu.findViewById(R.id.profile_group);
+        ItemParametres = guillotineMenu.findViewById(R.id.settins_group);
+        ItemDeconnection = guillotineMenu.findViewById(R.id.deconnection_group);
+        TextView textAc = (TextView) findViewById(R.id.accueil_group_text);
+        textAc.setTextColor(getResources().getColor(R.color.selected_item_color));
+
         if (toolbar != null) {
             setSupportActionBar(toolbar);
             getSupportActionBar().setTitle(null);
         }
 
-        View guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine, null);
-        root.addView(guillotineMenu);
 
+        TextView toolbar = (TextView) findViewById(R.id.toolbar_title);
         new GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), contentHamburger)
                 .setStartDelay(RIPPLE_DURATION)
                 .setActionBarViewForAnimation(toolbar)
                 .setClosedOnStart(true)
                 .build();
-        
+
+        onClickMenu(ItemAccueil,ItemAbsence,ItemParametres,ItemProfile,ItemDeconnection,toolbar);
+
+
+
 
     }
 
+    private void onClickMenu(View mViewAc,View mViewAb, View mViewPa, View mViewPr, View mViewDe, final TextView toolbar){
+
+        mViewAb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textAc = (TextView) findViewById(R.id.accueil_group_text);
+                TextView textAb = (TextView) findViewById(R.id.absence_group_text);
+                TextView textPa = (TextView) findViewById(R.id.settins_group_text);
+                TextView textPr = (TextView) findViewById(R.id.profile_group_text);
+                TextView textDe = (TextView) findViewById(R.id.deconnection_group_text);
+                toolbar.setText(getText(R.string.action_absences));
+                textAc.setTextColor(getResources().getColor(R.color.white));
+                textPa.setTextColor(getResources().getColor(R.color.white));
+                textPr.setTextColor(getResources().getColor(R.color.white));
+                textDe.setTextColor(getResources().getColor(R.color.white));
+                textAb.setTextColor(getResources().getColor(R.color.selected_item_color));
+                loadEtudiantAbsencesActivity();
+
+            }
+        });
+        mViewPr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView textAc = (TextView) findViewById(R.id.accueil_group_text);
+                TextView textAb = (TextView) findViewById(R.id.absence_group_text);
+                TextView textPa = (TextView) findViewById(R.id.settins_group_text);
+                TextView textPr = (TextView) findViewById(R.id.profile_group_text);
+                TextView textDe = (TextView) findViewById(R.id.deconnection_group_text);
+                toolbar.setText(getText(R.string.action_profile));
+                textAc.setTextColor(getResources().getColor(R.color.white));
+                textPa.setTextColor(getResources().getColor(R.color.white));
+                textPr.setTextColor(getResources().getColor(R.color.selected_item_color));
+                textDe.setTextColor(getResources().getColor(R.color.white));
+                textAb.setTextColor(getResources().getColor(R.color.white));
+                loadEtudiantProfileActivity();
+
+            }
+        });
+        mViewPa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView textAc = (TextView) findViewById(R.id.accueil_group_text);
+                TextView textAb = (TextView) findViewById(R.id.absence_group_text);
+                TextView textPa = (TextView) findViewById(R.id.settins_group_text);
+                TextView textPr = (TextView) findViewById(R.id.profile_group_text);
+                TextView textDe = (TextView) findViewById(R.id.deconnection_group_text);
+                toolbar.setText(getText(R.string.action_profile));
+                textAc.setTextColor(getResources().getColor(R.color.white));
+                textPa.setTextColor(getResources().getColor(R.color.selected_item_color));
+                textPr.setTextColor(getResources().getColor(R.color.white));
+                textDe.setTextColor(getResources().getColor(R.color.white));
+                textAb.setTextColor(getResources().getColor(R.color.white));
+                loadEtudiantSettingsActivity();
+            }
+        });
+        mViewDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadLoginActivity();
+            }
+        });
+    }
+    public void loadEtudiantAbsencesActivity(){
+        Intent myintent = new Intent(this, EtudiantAbsencesActivity.class);
+        startActivity(myintent);
+        finish();
+    }
+
+    public void loadEtudiantProfileActivity(){
+        Intent myintent = new Intent(this, EtudiantProfileActivity.class);
+        startActivity(myintent);
+        finish();
+    }
+    public void loadEtudiantSettingsActivity(){
+        Intent myintent = new Intent(this, EtudiantSettingsActivity.class);
+        startActivity(myintent);
+        finish();
+    }
+
+    public void loadLoginActivity(){
+        Intent myintent = new Intent(this, LoginActivity.class);
+        startActivity(myintent);
+        finish();
+    }
 
 }
 

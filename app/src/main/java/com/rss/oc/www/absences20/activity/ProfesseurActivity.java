468acc592bd28ae8c619377e69fc2e6a064fdd2c
CesarNg <bricecesar.ngoy-wessessi@epfedu.fr>
2017-06-07 22:45:11 +0200

    Layout Liste des absences page prof

--- app/src/main/java/com/rss/oc/www/absences20/activity/ProfesseurActivity.java
+++ app/src/main/java/com/rss/oc/www/absences20/activity/ProfesseurActivity.java
@@ -1,15 +1,83 @@
 package com.rss.oc.www.absences20.activity;
 
+import android.database.DataSetObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.rss.oc.www.absences20.R;
+import com.rss.oc.www.absences20.library.SlideExpandableListAdapter;
+import com.yalantis.guillotine.animation.GuillotineAnimation;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProfesseurActivity extends AppCompatActivity {
+
+    private static final long RIPPLE_DURATION = 250;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.root)
+    FrameLayout root;
+    @BindView(R.id.content_hamburger)
+    View contentHamburger;
+    View ItemAccueil;
+    View ItemAbsence;
+    View ItemProfile;
+    View ItemParametres;
+    View ItemDeconnection;
+    private String[] prenoms = new String[]{"Benoit","Brice","Yann"/*,"Jocelyn","Arthur","Glwadys","Olivier","Pierre","Jean"*/};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professeur);
+        ButterKnife.bind(this);
+        View guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine_prof, null);
+        root.addView(guillotineMenu);
+
+        ItemAccueil = guillotineMenu.findViewById(R.id.accueil_group);
+        ItemAbsence = guillotineMenu.findViewById(R.id.absence_group);
+        ItemProfile = guillotineMenu.findViewById(R.id.profile_group);
+        ItemParametres = guillotineMenu.findViewById(R.id.settins_group);
+        ItemDeconnection = guillotineMenu.findViewById(R.id.deconnection_group);
+
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(null);
+        }
+
+
+        TextView toolbar = (TextView) findViewById(R.id.toolbar_title);
+        new GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), contentHamburger)
+                .setStartDelay(RIPPLE_DURATION)
+                .setActionBarViewForAnimation(toolbar)
+                .setClosedOnStart(true)
+                .build();
+
+        ListView listView = (ListView)findViewById(R.id.list_absences);
+
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(ProfesseurActivity.this,
+           R.layout.list_absence_view,R.id.text ,prenoms);
+
+
+        listView.setAdapter( new SlideExpandableListAdapter(
+                listAdapter,
+                R.id.expandable_toggle_button,
+                R.id.expandable
+        ));
     }
 }
